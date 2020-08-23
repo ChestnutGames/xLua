7bd9c66f0000b673b07ac9bfc9178c26acc9e284
mephostopilis <mephostopilis@gmail.com>
2020-08-23 13:35:55 +0800

    去掉没有用的

--- build/lua/make_win32.bat
+++
@@ -1,5 +0,0 @@
-mkdir build32 & pushd build32
-cmake -G "Visual Studio 15 2017" ..
-popd
-cmake --build build32 --config Debug
-pause
--- build/lua/make_win64.bat
+++
@@ -1,5 +0,0 @@
-mkdir build64 & pushd build64
-cmake -G "Visual Studio 15 2017 Win64" ..
-popd
-cmake --build build64 --config Debug
-pause
